5b2ea420f09d7c93b52acd68cc5056d3e9629aac
CarlosXav <carlos.ed.sxs@gmail.com>
2022-04-27 20:36:37 +0000

    updated page

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    //setting up the values needed
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    //calculating the interest and final year
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    //validating the numeric input
+    if(isNaN(principal) || principal<=0){
+        //alerting user of the error made
+        window.alert("Enter a positive number");
+        //focusing back on the problematic field
+        document.getElementById("principal").focus();
+    }else{
+        //output the results
+        document.getElementById("result").innerHTML = "If you deposit <mark>" + principal +"</mark></br>"
+                                                    +"at an interest rate of <mark>" + rate +"</mark></br>"
+                                                    +"You will receive an amount of <mark>" + interest +"</mark></br>"
+                                                    +"in the year <mark>" + year +"</mark></br>";
+    }
+}
+
+//Updates the slider value when the user let go of it
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
